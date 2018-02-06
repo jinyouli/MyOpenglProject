1e181134e2235cff4023ca8a335dadd3dc18766a
Li JinYou <lijinyou@LideMacBook-Pro.local>
2018-02-06 17:58:30 +0800

    第一个opengl项目

---
+++ app/src/main/java/com/example/lijinyou/myopengltest1/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.lijinyou.myopengltest1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        glSurfaceView = new GLSurfaceView(this);
+
+        final ActivityManager activityManager =
+                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+
+        final ConfigurationInfo configurationInfo =
+                activityManager.getDeviceConfigurationInfo();
+
+        final boolean supportsEs2 =
+                configurationInfo.reqGlEsVersion >= 0x20000
+                        || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
+                        && (Build.FINGERPRINT.startsWith("generic")
+                        || Build.FINGERPRINT.startsWith("unknown")
+                        || Build.MODEL.contains("google_sdk")
+                        || Build.MODEL.contains("Emulator")
+                        || Build.MODEL.contains("Android SDK built for x86")));
+
+        if (supportsEs2) {
+            // Request an OpenGL ES 2.0 compatible context.
+            glSurfaceView.setEGLContextClientVersion(2);
+
+            // Assign our renderer.
+            glSurfaceView.setRenderer(new FirstOpenGLProjectRenderer( this));
+            rendererSet = true;
+        } else {
+            Toast.makeText(this, "This device does not support OpenGL ES 2.0.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        setContentView(glSurfaceView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (rendererSet) {
+            glSurfaceView.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (rendererSet) {
+            glSurfaceView.onResume();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lijinyou/myopengltest1/util/LoggerConfig.java
@@ -0,0 +1,10 @@
+package com.example.lijinyou.myopengltest1.util;
+
+/**
+ * Created by lijinyou on 2018/2/6.
+ */
+
+public class LoggerConfig {
+
+    public static final boolean ON = true;
+}
